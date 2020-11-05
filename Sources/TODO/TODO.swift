90ba3d985467f9d6fe244ffe3730fa2f8a6d3bf9
Ben Leggiero <BenLeggiero@Gmail.com>
2020-11-04 17:08:58 -0700

    Added a TODO which returns the context's expected type

--- Sources/TODO/TODO.swift
+++ Sources/TODO/TODO.swift
@@ -16,3 +16,14 @@
 public func TODO(_ message: StringLiteralType = "Not yet implemented") -> Never {
     fatalError("TODO: \(message)")
 }
+
+
+/// This immediately crashes. Use this when you haven't yet completed something, but you want it to compile because you
+/// want to test something else.
+///
+/// - Parameter message: _optional_ - The message to associate with the crash. Defaults to `"Not yet implemented"`
+/// - Returns: Never returns, but the compile-time type matches whatever you contextually need where this call is made
+@inlinable
+public func TODO<T>(_ message: String = "Not yet implemented") -> T {
+    fatalError("TODO: \(message)")
+}
